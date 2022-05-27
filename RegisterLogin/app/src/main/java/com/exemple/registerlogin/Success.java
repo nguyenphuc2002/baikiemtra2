c599f24d3011ec7321bd03f217bc1e735b2bbf1c
nguyenphuc2002 <97769890+nguyenphuc2002@users.noreply.github.com>
2022-05-27 19:49:54 +0700

    .

---
+++ RegisterLogin/app/src/main/java/com/exemple/registerlogin/Success.java
@@ -0,0 +1,14 @@
+package com.exemple.registerlogin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Success extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.success);
+    }
+}
